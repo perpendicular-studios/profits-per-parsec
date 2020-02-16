eb5d63c1ca831b4235b42f916424df11a5ff0ff8
Adrian Lam <adrianlamcw@gmail.com>
2020-02-16 14:13:41 -0500

    Solar System Prefab/ Basic Orbit code

---
+++ Assets/Scripts/SolarSystem/OrbitMotion.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitMotion : MonoBehaviour
+{
+    public Transform orbitingObject;
+    public Ellipse orbitPath;
+
+    [Range(0f, 1f)]
+    public float orbitProgress = 0f;
+    public float orbitPeriod = 3f;
+    public bool orbitActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (orbitingObject == null)
+        {
+            orbitActive = false;
+            return;
+        }
+        SetOrbitingObjectPosition();
+        StartCoroutine(AnimateOrbit());
+    }
+
+    void SetOrbitingObjectPosition()
+    {
+        Vector2 orbitPos = orbitPath.Evaluate(orbitProgress);
+        orbitingObject.localPosition = new Vector3(orbitPos.x, 0, orbitPos.y); 
+    }
+
+    IEnumerator AnimateOrbit()
+    {
+        if(orbitPeriod < 0.1f)
+        {
+            orbitPeriod = 0.1f;
+        }
+        float orbitSpeed = 1f / orbitPeriod;
+        while(orbitActive)
+        {
+            orbitProgress += Time.deltaTime * orbitSpeed;
+            orbitProgress %= 1f;
+            SetOrbitingObjectPosition();
+            yield return null;
+        }
+    }
+}
