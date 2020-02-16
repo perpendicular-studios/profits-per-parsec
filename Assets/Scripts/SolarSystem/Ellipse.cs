6e15bb0c05722eefc6d95ccdf3678e02e49962ab
Adrian Lam <adrianlamcw@gmail.com>
2020-02-16 01:27:51 -0500

    Started SolarSystem mechanics

---
+++ Assets/Scripts/SolarSystem/Ellipse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// To populate public fields from unity
+[System.Serializable]
+public class Ellipse
+{
+    // Initializing Variables
+    public float xAxis;
+    public float yAxis;
+
+    // Create ellipse with given size
+    public Ellipse (float xAxis, float yAxis)
+    {
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+    }
+
+    // Takes a time t and returns a position vector in the ellipse
+    public Vector2 Evaluate (float t)
+    {
+        float angle = Mathf.Deg2Rad * 360f * t;
+        float x = Mathf.Sin(angle) * xAxis;
+        float y = Mathf.Cos(angle) * yAxis;
+        return new Vector2(x, y);
+    }
+}
---
+++ Assets/Scripts/SolarSystem/EllipseRenderer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class EllipseRenderer : MonoBehaviour
+{
+    LineRenderer lr;
+
+    // Determines the number of segments (lines) that make up the ellipse 
+    [Range(3, 36)]
+    public int segments;
+    public Ellipse ellipse;
+
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        CalculateEllipse();
+    }
+
+    //
+    void CalculateEllipse()
+    {
+        Vector3[] points = new Vector3[segments + 1];
+
+        // Draws out each segment of the ellipse
+        for (int i = 0; i < segments; i++)
+        {
+            Vector2 position2D = ellipse.Evaluate((float)i / (float)segments);
+            points[i] = new Vector3(position2D.x, position2D.y, 0f);
+        }
+        points[segments] = points[0];
+
+        // Sets Line Renderer 
+        lr.positionCount = segments + 1;
+        lr.SetPositions(points);
+    }
+
+    void OnValidate()
+    {
+        if (Application.isPlaying && lr != null)
+        {
+            CalculateEllipse();
+        }
+        
+    }
+}
