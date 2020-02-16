97a961145f4f995e4df069a0cdf70ecbdf776674
Adrian Lam <adrianlamcw@gmail.com>
2020-02-16 15:53:22 -0500

    Added Mercury Placeholder Texture/ Planet Self Rotation script/ Show orbit paths

--- Assets/Scripts/SolarSystem/OrbitMotion.cs
+++ Assets/Scripts/SolarSystem/OrbitMotion.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Shows a physical path of the ellipse
+[RequireComponent(typeof(LineRenderer))]
 public class OrbitMotion : MonoBehaviour
 {
+    // Initiate Object Transfrom and Ellipse 
     public Transform orbitingObject;
     public Ellipse orbitPath;
 
+    // Initialize variables
     [Range(0f, 1f)]
     public float orbitProgress = 0f;
     public float orbitPeriod = 3f;
     public bool orbitActive = true;
+
+    // Sets number of segments that create the ellipse path
+    LineRenderer lr;
+    [Range(3, 72)]
+    public int segments;
+
+    // Called before start
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        CalculateEllipse();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +38,33 @@ public class OrbitMotion : MonoBehaviour
         }
         SetOrbitingObjectPosition();
         StartCoroutine(AnimateOrbit());
+    }
+
+    // Draws each segment in the ellipse
+    void CalculateEllipse()
+    {
+        Vector3[] points = new Vector3[segments + 1];
+
+        // Draws out each segment of the ellipse
+        for (int i = 0; i < segments; i++)
+        {
+            Vector2 position2D = orbitPath.Evaluate((float)i / (float)segments);
+            points[i] = new Vector3(position2D.x, 0f, position2D.y);
+        }
+        points[segments] = points[0];
+
+        // Sets Line Renderer 
+        lr.positionCount = segments + 1;
+        lr.SetPositions(points);
+    }
+
+    void OnValidate()
+    {
+        if (Application.isPlaying && lr != null)
+        {
+            CalculateEllipse();
+        }
+
     }
 
     void SetOrbitingObjectPosition()
---
+++ Assets/Scripts/SolarSystem/PlanetSelfRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSelfRotate : MonoBehaviour
+{
+    // Initializing Variables
+    public float rotationSpeed;      
+    public float dampAmt;            // Adjust rotation speed more finely
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate((Vector3.up * rotationSpeed) * (Time.deltaTime * dampAmt), Space.Self);
+    }
+}
