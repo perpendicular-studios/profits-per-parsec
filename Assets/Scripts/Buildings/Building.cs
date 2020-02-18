14969118088c1448dc16363028f7bd0f9f443d60
Andy Ma <solarcactus@live.com>
2020-02-17 21:58:11 -0800

    Renames, starting building system

---
+++ Assets/Scripts/Buildings/Building.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Building", order = 1)]
+public class Building : ScriptableObject
+{
+    public GameObject buildingPrefab;
+
+    [Header("Building Stats")]
+    public string displayName;
+    public string description;
+    public float cashPerSecond;
+}
---
+++ Assets/Scripts/Buildings/BuildingPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingPanel : MonoBehaviour
+{
+    public Building building;
+    public Text displayTitle;
+    public Text displayDescription;
+    public Image buildingImage;
+
+    public void Update()
+    {
+        if(building != null)
+        {
+            displayTitle.text = building.displayName;
+            displayDescription.text = building.description;
+            
+        }
+    }
+
+}
