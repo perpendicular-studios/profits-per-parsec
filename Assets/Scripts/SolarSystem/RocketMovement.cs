b4fd7bec94579408f6e14464422c875c60419a7a
Adrian Lam <adrianlamcw@gmail.com>
2020-02-19 20:26:40 -0500

    Added object detection and avoidance logic

--- Assets/Scripts/SolarSystem/RocketMovement.cs
+++ Assets/Scripts/SolarSystem/RocketMovement.cs
@@ -7,7 +7,12 @@ public class RocketMovement : MonoBehaviour
     [SerializeField] Transform target;
     [SerializeField] Transform startPosition;
     [SerializeField] float movementSpeed;
-    [SerializeField] float rotationalSpeed; //for collision make sure rotationalSpeed is 10x of movementSpeed
+    [SerializeField] float rotationalSpeed;               //for collision make sure rotationalSpeed is 10x of movementSpeed
+    [SerializeField] float detectionDistance;             //raycast detection distance
+    [SerializeField] float rayCastOffset;                 //raycast distance from center of rocket
+    [SerializeField] float bufferDuration;                //sets rotateBuffer
+    [SerializeField] float emergencyDir;                  //angle of turn when object is detected (20 seems to be good)
+    float rotateBuffer = 0;                               //how long emergency direction turning will last for when object is detected
 
     // Start is called at beginning
     void Start()
@@ -18,10 +23,11 @@ public class RocketMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Turn();
+        Pathfinding();
         Move();
     }
 
+    // Turning logic, rotates rocket to face target object location each frame
     void Turn()
     {
         Vector3 pos = target.position - transform.position;
@@ -29,8 +35,70 @@ public class RocketMovement : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationalSpeed * Time.deltaTime);
     }
 
+    // Moves rocket forward by given movementSpeed
     void Move()
     {
         transform.position += transform.forward * movementSpeed* Time.deltaTime;
+    }
+
+    // Pathfinding logic
+    void Pathfinding()
+    {
+        RaycastHit hit;
+        Vector3 hitOffset = Vector3.zero;
+
+        // Initialize Raycasts positions on rocket
+        Vector3 left = transform.position - transform.right * rayCastOffset;
+        Vector3 right = transform.position + transform.right * rayCastOffset;
+        Vector3 up = transform.position + transform.up * rayCastOffset;
+        Vector3 down = transform.position - transform.up * rayCastOffset;
+
+        // For debugging, draws out raycasts from rockets
+        Debug.DrawRay(left, transform.forward * detectionDistance, Color.red);
+        Debug.DrawRay(right, transform.forward * detectionDistance, Color.red);
+        Debug.DrawRay(up, transform.forward * detectionDistance, Color.red);
+        Debug.DrawRay(down, transform.forward * detectionDistance, Color.red);
+        
+        if (Physics.Raycast(left, transform.forward, out hit, detectionDistance) && !isTarget(hit))
+        {
+            hitOffset += emergencyDir * Vector3.right;
+            hitOffset += emergencyDir * Vector3.up;
+        }
+        else if (Physics.Raycast(right, transform.forward, out hit, detectionDistance) && !isTarget(hit))
+        {
+            hitOffset -= emergencyDir * Vector3.right;
+            hitOffset += emergencyDir * Vector3.up;
+        }
+        if (Physics.Raycast(up, transform.forward, out hit, detectionDistance) && !isTarget(hit))
+        {
+            hitOffset -= emergencyDir * Vector3.up;
+        }
+            
+        else if (Physics.Raycast(down, transform.forward, out hit, detectionDistance) && !isTarget(hit))
+        {
+            hitOffset += emergencyDir * Vector3.up;
+        }
+        if (rotateBuffer!=0)
+        {
+            transform.Rotate(hitOffset * emergencyDir * Time.deltaTime);
+            rotateBuffer -= 1;
+        }
+        else if (hitOffset != Vector3.zero )
+        {
+            rotateBuffer = bufferDuration;
+        }
+        else
+            Turn();
+    }
+
+    // Returns true if raycast hits target position
+    bool isTarget(RaycastHit hit)
+    {
+        Debug.Log(hit.transform.gameObject);
+        if(startPosition != null)
+        {
+            return hit.transform.gameObject == target.gameObject || hit.transform.gameObject == startPosition.gameObject; 
+        }
+        return hit.transform.gameObject == target.gameObject;
     }
 }
