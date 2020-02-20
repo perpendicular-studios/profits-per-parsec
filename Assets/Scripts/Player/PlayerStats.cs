5eb2981a32f6b6920b053338e24084e8b919b103
Andy Ma <solarcactus@live.com>
2020-02-19 21:53:05 -0800

    More building code, added player folder and a PlayerStats object

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int currencyAmount;
+    public float growthRate;
+    public int publicRelationAmount;
+}
