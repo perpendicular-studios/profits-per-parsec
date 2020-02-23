89980f5a4168231eb5b4ab3bf274138d401d3eea
Andy Ma <solarcactus@live.com>
2020-02-22 19:36:49 -0800

    Rockets now are infinite - they will launch every certain amount of seconds from startPosition to target.

--- Assets/Scripts/SolarSystem/RocketEvents.cs
+++ Assets/Scripts/SolarSystem/RocketEvents.cs
@@ -6,28 +6,36 @@ using System;
 public class RocketEvents : MonoBehaviour
 {
     public GameObject rocket;
-    public static int numRockets = 0;
+    public float rocketLaunchTimeDelay = 10;
+
+    private static int numRockets = 0;
     private static string target;
     private static string startPosition;
+    private float actionTime;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
+    {
+        actionTime = rocketLaunchTimeDelay;
+    }
+
+    void OnEnable()
     {
         RocketController.OnRocketLaunch += Launch;
         RocketMovement.OnRocketLand += Land;
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(numRockets);
-        //Invoke Launch Rocket event 
-        if (numRockets>0)
+        actionTime += Time.deltaTime;
+        if (numRockets > 0 && actionTime > rocketLaunchTimeDelay)
         {
             var newRocket = Instantiate(rocket);
             newRocket.GetComponent<RocketMovement>().startPosition = GameObject.Find(startPosition).transform;
             newRocket.GetComponent<RocketMovement>().target = GameObject.Find(target).transform;
-            numRockets--;
+
+            actionTime = 0;
         }
     }
 
