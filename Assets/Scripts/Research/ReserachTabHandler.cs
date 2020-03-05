a42bb2725ad5d25e2d5db23479b1bf5d55359781
Adrian Lam <adrianlamcw@gmail.com>
2020-03-05 17:55:01 -0500

    press t for research/ can switch between research tabs

---
+++ Assets/Scripts/Research/ReserachTabHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReserachTabHandler : MonoBehaviour
+{
+    public List<ResearchTabButton> tabButtons;
+    public Color tabIdle;
+    public Color tabHover;
+    public Color tabActive;
+    public ResearchTabButton selectedTab;
+    public List<GameObject> pagesToSwap;
+
+    public void Subscribe(ResearchTabButton button)
+    {
+        if(tabButtons == null)
+        {
+            tabButtons = new List<ResearchTabButton>();
+        }
+
+        tabButtons.Add(button);
+    }
+    public void OnTabEnter(ResearchTabButton button)
+    {
+        ResetTabs();
+        if(selectedTab == null || button != selectedTab)
+        {
+            button.GetComponent<Image>().color = tabHover;
+        }
+    }
+
+    public void OnTabExit(ResearchTabButton button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(ResearchTabButton button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        button.GetComponent<Image>().color = tabActive;
+        int index = button.transform.GetSiblingIndex();
+        for(int i=0; i < pagesToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                pagesToSwap[i].SetActive(true);
+            }
+            else
+            {
+                pagesToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach(ResearchTabButton button in tabButtons)
+        {
+            if (selectedTab != null && button == selectedTab) { continue; }
+            button.GetComponent<Image>().color = tabIdle;
+        }
+    }
+}
