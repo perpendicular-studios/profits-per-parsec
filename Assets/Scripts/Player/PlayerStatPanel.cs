791a53f80d0871e5d3b188f2608d9951ea00a972
Andy Ma <solarcactus@live.com>
2020-08-01 13:55:43 -0700

    display changes + notification changes

--- Assets/Scripts/Player/PlayerStatPanel.cs
+++ Assets/Scripts/Player/PlayerStatPanel.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class PlayerStatPanel : MonoBehaviour
 {
     public Image iconImage;
     public Text valueText;
+    public GameObject tooltip;
 
     public void Awake()
     {
+        tooltip.SetActive(false);
         iconImage = GetComponentInChildren<Image>();
         valueText = GetComponentInChildren<Text>();
     }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltip.SetActive(true);
+        Debug.Log("Hello!");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.SetActive(false);
+    }
+
 }
